fe214349e99452907e2f8b6d51d9ef2354d8387e
Mehmet Uysal <mehmetuysal2000@yandex.com>
2025-07-10 19:43:12 +0300

    readme update

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # UAuth Test App
 
 This temaplate shows how to use UAuth in a Vue app.
+
+Used JS Package can be found in [github](https://github.com/uys2000-javascript-package/uauth).  
+Used PHP backend service can be found in [github](https://github.com/uys2000-service/php-services).
+
+repo links:  
+https://github.com/uys2000-javascript-package/uauth
+https://github.com/uys2000-service/php-services  
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <div>
+      <div style="text-align:  center; color: red;">check console and network for more info</div>
+      <span>Init Services</span>
       <select v-model="env">
         <option value="local">local</option>
         <option value="test">test</option>
@@ -8,7 +10,7 @@
       <button @click="init">init</button>
     </div>
     <div>
-
+      <span>Auth Service</span>
       <input type="text" v-model="name" placeholder="name">
       <input type="text" v-model="email" placeholder="name">
       <input type="text" v-model="password" placeholder="name">
@@ -22,42 +24,95 @@
       <button @click="signout">sign out</button>
       <button @click="signoff">sign off</button>
     </div>
+    <div>
+      <span>Session Service</span>
+      <button @click="check">Check Session</button>
+      <button @click="list">List Session</button>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>token</th>
+              <th>ip</th>
+              <th>platform</th>
+              <th>timestamp</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="session, index in sessions" :key="index">
+              <td>{{ session.token }}</td>
+              <td>{{ session.ip }}</td>
+              <td>{{ session.platform }}</td>
+              <td>{{ session.timestamp }}</td>
+              <td style="text-align: center;">
+                <button @click="() => remove(session.token)">remove</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="info">
+      {{ info }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { UAuth, UHost } from "u-auth"
+import { UAuthService, UHost, USessionService, type USession } from "u-auth"
 export default {
   data() {
     return {
-      auth: new UAuth(new UHost("local")),
+      authService: new UAuthService(new UHost("local", "auth")),
+      sessionService: new USessionService(new UHost("local", "session")),
       env: "local" as "local" | "test",
       name: "test",
       email: "test@test.com",
       password: "testpass",
-      platform: "web" as "web" | "android" | "ios"
+      platform: "web" as "web" | "android" | "ios",
+      sessions: [] as USession[],
+      info: ""
     }
   },
   methods: {
     init() {
-      this.auth = new UAuth(new UHost(this.env))
+      this.authService = new UAuthService(new UHost(this.env, "auth"))
+      this.sessionService = new USessionService(new UHost(this.env, "session"))
     },
-    signin() {
-      this.auth.signIn(this.email, this.password, this.platform).then(token => {
-        alert(token)
-      })
+    async signin() {
+      const token = await this.authService.signIn(this.email, this.password, this.platform)
+      this.sessionService.token = token;
+      this.info = `signed in, token :${token}`;
     },
-    signup() {
-      this.auth.signUp(this.name, this.email, this.password, this.platform).then(token => {
-        document.cookie = `token=${token}; path=/; SameSite=Lax`;
-        alert(token)
-      })
+    async signup() {
+      const token = await this.authService.signUp(this.name, this.email, this.password, this.platform)
+      this.sessionService.token = token;
+      this.info = `signed up, token :${token}`;
     },
-    signout() {
-      this.auth.signOut().then((res) => alert(JSON.stringify(res)))
+    async signout() {
+      const result = await this.authService.signOut()
+      this.sessionService.token = "";
+      this.info = `signed out, reusult: ${result}`;
     },
-    signoff() {
-      this.auth.signOff(this.email, this.password, this.platform).then((res) => alert(JSON.stringify(res)))
+    async signoff() {
+      const result = await this.authService.signOff(this.email, this.password, this.platform)
+      this.sessionService.token = "";
+      this.info = `signed off, reusult: ${result}`;
+    },
+    async check() {
+      const result = await this.sessionService.check()
+      this.info = `session checked, reusult: ${result}`;
+    },
+    async list() {
+      const result = await this.sessionService.list()
+      this.sessions = result;
+      this.info = `session listed, session count: ${result.length}`;
+    },
+    async remove(token: string) {
+      const result = await this.sessionService.remove(token)
+      this.info = `session removed REFRESH LIST, reusult: ${result}`;
+      if (token == this.authService.token) this.authService.token = ""
     }
   },
   mounted() {
@@ -70,5 +125,22 @@ div {
   display: flex;
   flex-direction: column;
   padding: 4px;
+}
+
+div:has(>table) {
+  max-width: 100%;
+  overflow: auto;
+}
+
+table,
+th,
+td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+
+.info {
+  position: fixed;
+  bottom: 0;
 }
 </style>
